170c83239b9c54fcf73afd538e1a9b18cac3559c
Karganleh  Borh <karganlehborh@Karganlehs-MacBook-Pro.local>
2021-02-18 18:27:22 -0600

    final commit

--- schema..sql
+++ schema..sql
@@ -8,13 +8,13 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY ( emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -23,23 +23,30 @@ CREATE TABLE salaries (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, from_date)
 );
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	 PRIMARY KEY (dept_no)
+	 PRIMARY KEY (emp_no,dept_no)
 );
 CREATE TABLE titles(
+	emp_no INT NOT NULL,
 	title VARCHAR(50) NOT NULL,
-    emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	 PRIMARY KEY (title)
+	 PRIMARY KEY (emp_no, title, from_date)
 );
 select*from departments;
+
+CREATE TABLE departments(
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY(dept_no)
+
+);
